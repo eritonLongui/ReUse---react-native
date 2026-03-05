2110fd29b5628e10470139ef7ae676dddf21eec8
eritonLongui <7eriton@gmail.com>
2026-03-04 21:35:37 -0300

    Ajustando Base do Projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles.ts';
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Tela Home</Text>
+    </View>
+  );
+}
